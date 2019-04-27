74f43b2ee71cf661708d1fe2918ef4a923b1317f
Raghava2016 <raghava.juvvaji@gmail.com>
2019-04-26 19:56:21 -0700

    project skeleton

---
+++ restaurant-service/src/main/java/org/learning/trs/RestaurantServiceApplication.java
@@ -0,0 +1,14 @@
+package org.learning.trs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class RestaurantServiceApplication {
+    public static void main(String args[]) {
+        SpringApplication.run(RestaurantServiceApplication.class, args);
+
+    }
+}
